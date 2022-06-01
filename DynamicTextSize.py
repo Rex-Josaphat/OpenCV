d8034885b6571de6ec3612925158ce6d7c308142
Josaphat NGOGA UWIZEYE <josaphatngogauwizeye@gmail.com>
2022-06-01 12:04:53 +0200

    initial commit

--- DynamicTextSize.py
+++ DynamicTextSize.py
@@ -3,12 +3,20 @@ It also includes a dynamic text adjustment that changes size f the text dependin
 import cv2
 import cvzone
 from cvzone.FaceMeshModule import FaceMeshDetector
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 detector = FaceMeshDetector(maxFaces=1)
 
+textList = ['Hello, My name is Rex',
+            'I live at Ruyenzi.',
+            "I'm a big fan of computer programming.",
+            'My favorite language is Python.',
+            "I do believe I'm very good at it"]
+
 while True:
     success, img = cap.read()
+    imgText = np.zeros_like(img)
     img = cv2.flip(img, 1)
     img, faces = detector.findFaceMesh(img, draw= False)
 
@@ -25,21 +33,18 @@ while True:
         w, _ = detector.findDistance(pointLeft, pointRight) #Distance in pixels of image in camera
         W = 6.3 # Average distance between the left and right eye
 
-        # Finding the focal length of the camera
-        # d = 50 # Estimated distance between the face and camera to help us get the value of the focal lenght
-        #f = (w*d)/W # Formula to find focal length
-        #print(f)
-        '''After finding the focal length a fixed value is recorded so as to be used in other calculations
-        and we comment out this calculating part'''
-
-        # Finding the distance between camera and face
         f = 840
         d = int((W*f)/w)
         print(d)
 
         cvzone.putTextRect(img, f'Distance: {d}cm', (face[10][0]-130, face[10][0]-225), scale=1.8)
 
-    cv2.imshow("Face-Camera Distance", img)
+        for i, text in enumerate(textList):
+            singleHeight = 50
+            cv2.putText(imgText, text, (50, 50 + (i*singleHeight)), cv2.FONT_ITALIC, 2, (255, 255, 255), 2)
+
+    imgStacked = cv2.stackImages([img, imgText], 2, 1)
+    cv2.imshow("Face-Camera Distance", imgStacked)
     if cv2.waitKey(1) == ord('q'):
         break
 cap.release()
